8efbb424785016b4c3b698a7d1b976bb48dca385
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:24:28 +0700

    menambahkan komponen TopAppBar reusable

---
+++ app/src/main/java/com/example/etherealwardrobe/view/WardrobeTopAppBar.kt
@@ -0,0 +1,31 @@
+package com.example.etherealwardrobe.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WardrobeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {}
+) {
+    TopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        }
+    )
+}
