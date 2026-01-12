cf57d9f1bcdb47ff261b719c4c6355fb01da930c
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:32:08 +0700

    menambahkan MainActivity dengan setup Jetpack Compose dan Navigation

---
+++ app/src/main/java/com/example/etherealwardrobe/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.etherealwardrobe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.etherealwardrobe.ui.theme.EtherealWardrobeTheme
+import com.example.etherealwardrobe.uicontrollerimport.PetaNavigasi
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EtherealWardrobeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    PetaNavigasi(navController = navController)
+                }
+            }
+        }
+    }
+}
