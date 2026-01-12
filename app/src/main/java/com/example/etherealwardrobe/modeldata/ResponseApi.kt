39a828657bd0d1d1545baa7d45b71750f983400f
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:10:07 +0700

    menambahkan response API

---
+++ app/src/main/java/com/example/etherealwardrobe/modeldata/ResponseApi.kt
@@ -0,0 +1,14 @@
+package com.example.etherealwardrobe.modeldata
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseApi<T>(
+    @SerializedName("status")
+    val status: String,
+
+    @SerializedName("message")
+    val message: String,
+
+    @SerializedName("data")
+    val data: T? = null
+)
