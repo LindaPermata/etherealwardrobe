41fb47d78c43f358ea3fbdbf4e756e7472b84acc
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:17:38 +0700

    menambahkan destinasi navigasi form produk

---
+++ app/src/main/java/com/example/etherealwardrobe/uicontroller/route/DestinasiFormProduk.kt
@@ -0,0 +1,9 @@
+package com.example.etherealwardrobe.uicontroller.route
+
+object DestinasiFormProduk : DestinasiNavigasi {
+    override val route = "form_produk"
+    override val title = "Form Produk"
+    const val isEditArg = "isEdit"
+    const val idKategoriArg = "idKategori"
+    const val idProdukArg = "idProduk"
+}
