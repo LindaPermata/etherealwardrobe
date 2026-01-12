adc3701bd7238159b2789d7456a8af3bdb8363f1
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:17:54 +0700

    menambahkan destinasi navigasi list produk

---
+++ app/src/main/java/com/example/etherealwardrobe/uicontroller/route/DestinasiListProduk.kt
@@ -0,0 +1,8 @@
+package com.example.etherealwardrobe.uicontroller.route
+
+
+object DestinasiListProduk : DestinasiNavigasi {
+    override val route = "list_produk"
+    override val title = "Daftar Produk"
+    const val idKategoriArg = "idKategori"
+}
