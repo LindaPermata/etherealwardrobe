4d05ebe69de209307af23d55560c8356555cece1
LindaPermata <permatalinda531@gmail.com>
2026-01-12 10:17:26 +0700

    menambahkan destinasi navigasi detail produk

---
+++ app/src/main/java/com/example/etherealwardrobe/uicontroller/route/DestinasiDetailProduk.kt
@@ -0,0 +1,8 @@
+package com.example.etherealwardrobe.uicontroller.route
+
+
+object DestinasiDetailProduk : DestinasiNavigasi {
+    override val route = "detail_produk"
+    override val title = "Detail Produk"
+    const val idProdukArg = "idProduk"
+}
